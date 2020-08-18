469f09b19ab7d4089cb05dd0183e3e802c415f06
yeidan79 <1181241@isep.ipp.pt>
2020-08-18 16:08:40 +0100

    Updates

---
+++ precache-manifest.998312fcfac2707a1e769851c043399f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1c944c96455f02d62c1d9e44dd4a1f",
+    "url": "/voice-app-v2/index.html"
+  },
+  {
+    "revision": "afb2a075a6ea7a48f1b0",
+    "url": "/voice-app-v2/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e0e890a550ae9d90f69d",
+    "url": "/voice-app-v2/static/css/main.3a0026ed.chunk.css"
+  },
+  {
+    "revision": "afb2a075a6ea7a48f1b0",
+    "url": "/voice-app-v2/static/js/2.b76293bc.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/voice-app-v2/static/js/2.b76293bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e890a550ae9d90f69d",
+    "url": "/voice-app-v2/static/js/main.c1c76fb8.chunk.js"
+  },
+  {
+    "revision": "11ec2fe163fe0e4b4fae",
+    "url": "/voice-app-v2/static/js/runtime-main.972342c5.js"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/voice-app-v2/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "2719760172113d02d4f90892f5ef4ed3",
+    "url": "/voice-app-v2/static/media/cool-background.27197601.png"
+  },
+  {
+    "revision": "c0caed933bd0241a821b021291339182",
+    "url": "/voice-app-v2/static/media/futuroNormal.c0caed93.ttf"
+  },
+  {
+    "revision": "6d79d6c1605e89f6bc0479272447e5f3",
+    "url": "/voice-app-v2/static/media/mars.6d79d6c1.jpg"
+  },
+  {
+    "revision": "0c315261a0a4495369a40931df123787",
+    "url": "/voice-app-v2/static/media/space1.0c315261.jpg"
+  },
+  {
+    "revision": "f105027824abcef5361d3cfbf921fe91",
+    "url": "/voice-app-v2/static/media/space3.f1050278.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/voice-app/precache-manifest.21034ea9529e822814d5aa13cbb03fcd.js"
+  "/voice-app-v2/precache-manifest.998312fcfac2707a1e769851c043399f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/voice-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/voice-app-v2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
